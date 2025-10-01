82d1d2e8a1cb89cb0952d5e16f4e8c8784f28ad4
mjk-hudson <majik.hudson@icloud.com>
2025-09-30 23:46:19 -0400

    Added the heading input event handler and updated the component add button

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -4,20 +4,34 @@ import './TodoList.css';
 const TodoList = () => {
   const [todos,setTodos] = useState([]);
   const [headingInput,setheadingInput]= useState("");
-  const [listInput,setlistInput]= useState({});  
+  const [listInput,setlistInput]= useState({});
+
+  /*Add the heading by checking whether or not the input is empty
+  if empty create a new todos object with a headind and an empty list array
+  the reset the heading input to empty string*/
+  const handleAddTodo = () => {
+    if(headingInput.trim() !==''){
+      setTodos([...todos, {heading: headingInput, list: []}]);
+      setheadingInput('');
+    }
+  }
 
   return (
     <>
       <div className="todo-container">
         <h1 className="title">My Todo List</h1>
         <div className="input-container">
+          {/*Input field to enter new heading*/}
           <input
             type="text"
-            className="heading-input"
-            placeholder="Enter heading"
-            
+            className="heading-input"//CSS Class for styling
+            placeholder="Enter heading"//Text shown when input is empty
+            value= {headingInput}
+            onChange={(e)=> {setheadingInput(e.target.value);}}//Add onChange event to update the headingInput state
           />
-          <button className="add-list-button">Add Heading</button>
+          {/*Button to add new heading*/}
+          <button className="add-list-button"
+          onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
